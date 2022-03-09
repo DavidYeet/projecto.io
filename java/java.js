5878cdd94bbb3a2a7ef0fa9525f3259aa6748538
David131 <56269004+DavidYeet@users.noreply.github.com>
2022-03-09 13:42:56 -0500

    Add files via upload

--- java/java.js
+++ java/java.js
@@ -1,17 +1,20 @@
 const app = Vue.createApp({
-  //template: '<h2>I am the Template</h2>'
   data() {
     return {
+      showBooks: true,
       title: 'Bob The Musical',
       author: 'Dr. Bob',
       age: 3
     }
   },
-  methods: {
-    changeTitle() {
-      this.title = 'Words of Randiance'
+
+methods: {
+toggleShowBooks(){
+  this.showBooks = !this.showBooks
+}
+    // changeTitle() {
+    //   this.title = 'Words of Randiance'
     }
-  }
 })
 
 app.mount('#app')
