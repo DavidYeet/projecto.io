bedc6686671c4c96c3f2f1704f90cef04266e754
David131 <56269004+DavidYeet@users.noreply.github.com>
2022-03-11 12:52:28 -0500

    Add files via upload

--- java/java.js
+++ java/java.js
@@ -4,14 +4,26 @@ const app = Vue.createApp({
       showBooks: true,
       title: 'Bob The Musical',
       author: 'Dr. Bob',
-      age: 3
+      age: 3,
+      x: 0,
+      y: 0,
     }
   },
 
 methods: {
 toggleShowBooks(){
   this.showBooks = !this.showBooks
-}
+},
+handleEvent(e, data) {
+  console.log(e,e.type)
+  if (data) {
+    console.log(data)
+  }
+},
+handleMousemove(e) {
+  this.x = e.offsetX
+  this.y = e.offsetY
+  }
 }
 
 })
